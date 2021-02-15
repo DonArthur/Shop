289656f965bc4de24589f0811b0a54f9f7614387
Geraldy <geraldy.miero@gmail.com>
2021-02-16 02:31:37 +0700

    Get All Products

--- Shop.Application/Products/GetProducts.cs
+++ Shop.Application/Products/GetProducts.cs
@@ -1,10 +1,32 @@
-﻿using System;
+﻿using Shop.Database;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
-namespace Shop.Application.Products
+namespace Shop.Application.GetProducts
 {
     public class GetProducts
     {
+        private ApplicationDbContext _ctx;
+
+        public GetProducts(ApplicationDbContext ctx)
+        {
+            _ctx = ctx;
+        }
+        public IEnumerable<ProductViewModel> Do() => 
+            _ctx.Products.ToList().Select(x => new ProductViewModel
+            {
+                Name = x.Name,
+                Description = x.Description,
+                Value = $"Rp.{x.Value.ToString("N2")}"
+            });
     }
+    public class ProductViewModel
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Value { get; set; }
+    }
+
 }
--- Shop.UI/Pages/Index.cshtml.cs
+++ Shop.UI/Pages/Index.cshtml.cs
@@ -1,7 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
-using Shop.Application.Products;
+using Shop.Application.CreateProducts;
+using Shop.Application.GetProducts;
 using Shop.Database;
 using System;
 using System.Collections.Generic;
@@ -19,20 +20,16 @@ namespace Shop.UI.Pages
             _ctx = ctx;
         }
         [BindProperty]
-        public ProductViewModel Product { get; set; }
-        public class ProductViewModel
-        {
-            public string Name { get; set; }
-            public string Description { get; set; }
-            public decimal Value { get; set; }
-        }
+        public Application.CreateProducts.ProductViewModel Product { get; set; }
+        public IEnumerable<Application.GetProducts.ProductViewModel> Products { get; set; }
 
         public void OnGet()
         {
+            Products = new GetProducts(_ctx).Do();
         }
         public async Task<IActionResult> OnPost()
         {
-            await new CreateProduct(_ctx).Do(Product.Name, Product.Description, Product.Value);
+            await new CreateProduct(_ctx).Do(Product);
             return RedirectToPage("Index");
         }
     }
