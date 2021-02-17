884b42cc507afdbad1e47ae2ae4658bd01ae040f
Geraldy <geraldy.miero@gmail.com>
2021-02-17 23:38:10 +0700

    Add VueJS

---
+++ Shop.UI/wwwroot/js/admin/main.js
@@ -0,0 +1,20 @@
+﻿var app = new Vue({
+    el: '#app',
+    data: {
+        price: 0,
+        showPrice: true
+    },
+    methods: {
+        togglePrice() {
+            this.showPrice = !this.showPrice
+        }
+    },
+    computed: {
+        formatPrice() {
+            return 'Rp.' + this.price
+        },
+        hidePrice() {
+            return 'Rp.' + this.price.replace(this.price, '*'.repeat(this.price.length))
+        }
+    }
+})
